43e72696872baebb6268e73f7eae547caa02a202
babalawo-ife <oraclegod@icloud.com>
2024-04-01 10:05:58 +0100

    2 commit

--- head_or_tail.py
+++ head_or_tail.py
@@ -3,3 +3,4 @@ import random
 random_int = random.getrandbits(100)
 
 print(random_int)
+ 
--- random_generator.py
+++ random_generator.py
@@ -1,4 +1,4 @@
 import random
 
 random_int = random.randint(100, 9000)
-print(f"Your luck number is {random_int}")
+print(f"Your lucky number is {random_int}")
